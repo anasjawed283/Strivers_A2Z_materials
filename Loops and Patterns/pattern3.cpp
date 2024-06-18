4b5320afacdd6592fa2a4d95e55edf7bb8952233
Anas Jawed <anasjawed283@gmail.com>
2024-06-18 23:46:09 +0530

    Patterns

---
+++ Loops and Patterns/pattern3.cpp
@@ -0,0 +1,23 @@
+/*
+
+1
+12
+123
+1234
+12345
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n=5;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<(j+1);
+        }
+        cout<<endl;
+    }
+}
