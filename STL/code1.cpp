40ed5d26f02bf401a5b1457d46be3ab109cb7e7a
Anas Jawed <anasjawed283@gmail.com>
2024-06-24 22:38:23 +0530

    STL

---
+++ STL/code1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//PAIRS ----------
+
+int main(){
+    pair<int, int>p ={1,3};
+    cout<<p.first<<" "<<p.second<<endl; //1 3
+
+    pair<int, pair<int,int>>p2={1,{2,3}}; // store 3 elements in a pair
+    cout<<p2.first<<" "<<p2.second.first<<" "<<p2.second.second<<endl;
+
+    pair<int,int>arr[] = {{1,2},{3,4},{5,6}}; //4 is at index 1.second
+    cout<< arr[1].second<<endl;
+}
