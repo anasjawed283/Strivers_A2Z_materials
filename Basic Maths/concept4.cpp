240cbc97c2a324ac4a21140c5e888b656f8f4f3f
Anas Jawed <anasjawed283@gmail.com>
2024-07-10 23:56:38 +0530

    Basic maths

---
+++ Basic Maths/concept4.cpp
@@ -0,0 +1,25 @@
+// ARRMSTRONG NUMBER  //
+/*
+
+An Armstrong number
+(also known as a narcissistic number or a pluperfect digital invariant) is a number that is equal to the sum of its own digits each raised to the power of the number of digits. 
+Examples 3-digit Armstrong Number:
+153: 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153 1 3 +5 3 +3 3 =1+125+27=153 
+370: 3^3 + 7^3 + 0^3 = 27 + 343 + 0 = 370 3 3 +7 3 +0 3 =27+343+0=370 
+371: 3^3 + 7^3 + 1^3 = 27 + 343 + 1 = 371 3 3 +7 3 +1 3 =27+343+1=371 
+407: 4^3 + 0^3 + 7^3 = 64 + 0 + 343 = 407 4 3 +0 3 +7 3 =64+0+343=407 
+
+4-digit Armstrong Number: 
+1634: 1^4 + 6^4 + 3^4 + 4^4 = 1 + 1296 + 81 + 256 = 1634 1 4 +6 4 +3 4 +4 4 =1+1296+81+256=1634
+
+*/
+
+#include<iostream>
+using namespace std;
+
+void check_armstrong(int n){
+    int num =n;
+    int sum =0;
+
+    while()
+}
